22a61fda64e2c4353b675012d413f67572d0d7db
Elrazi KOULA <elrazi.koula@viacesi.fr>
2023-12-05 09:11:47 +0100

    Ajoutez des fichiers projet.

---
+++ Interface1/MainForm.h
@@ -0,0 +1,215 @@
+#pragma once
+
+namespace WinProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Description résumée de MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ tbFirstName;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ tbLastName;
+	private: System::Windows::Forms::Label^ lbWelcome;
+
+
+
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
+	private: System::Windows::Forms::Button^ btnOK;
+	private: System::Windows::Forms::Button^ btnClear;
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->tbFirstName = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->tbLastName = (gcnew System::Windows::Forms::TextBox());
+			this->lbWelcome = (gcnew System::Windows::Forms::Label());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->btnOK = (gcnew System::Windows::Forms::Button());
+			this->btnClear = (gcnew System::Windows::Forms::Button());
+			this->tableLayoutPanel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(119, 35);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Identifiant";
+			// 
+			// tbFirstName
+			// 
+			this->tbFirstName->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tbFirstName->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tbFirstName->Location = System::Drawing::Point(18, 47);
+			this->tbFirstName->Name = L"tbFirstName";
+			this->tbFirstName->Size = System::Drawing::Size(375, 43);
+			this->tbFirstName->TabIndex = 1;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(15, 117);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(144, 35);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Mot de passe";
+			// 
+			// tbLastName
+			// 
+			this->tbLastName->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tbLastName->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tbLastName->Location = System::Drawing::Point(18, 167);
+			this->tbLastName->Name = L"tbLastName";
+			this->tbLastName->Size = System::Drawing::Size(375, 43);
+			this->tbLastName->TabIndex = 3;
+			// 
+			// lbWelcome
+			// 
+			this->lbWelcome->AutoSize = true;
+			this->lbWelcome->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbWelcome->Location = System::Drawing::Point(21, 258);
+			this->lbWelcome->Name = L"lbWelcome";
+			this->lbWelcome->Size = System::Drawing::Size(76, 35);
+			this->lbWelcome->TabIndex = 4;
+			this->lbWelcome->Text = L"label3";
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel1->ColumnCount = 2;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->Controls->Add(this->btnOK, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->btnClear, 1, 0);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(27, 389);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 1;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(366, 53);
+			this->tableLayoutPanel1->TabIndex = 5;
+			// 
+			// btnOK
+			// 
+			this->btnOK->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnOK->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnOK->Location = System::Drawing::Point(3, 3);
+			this->btnOK->Name = L"btnOK";
+			this->btnOK->Size = System::Drawing::Size(177, 45);
+			this->btnOK->TabIndex = 0;
+			this->btnOK->Text = L"OK";
+			this->btnOK->UseVisualStyleBackColor = true;
+			this->btnOK->Click += gcnew System::EventHandler(this, &MainForm::btnOK_Click);
+			// 
+			// btnClear
+			// 
+			this->btnClear->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->btnClear->Font = (gcnew System::Drawing::Font(L"Segoe Print", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnClear->Location = System::Drawing::Point(186, 3);
+			this->btnClear->Name = L"btnClear";
+			this->btnClear->Size = System::Drawing::Size(177, 45);
+			this->btnClear->TabIndex = 1;
+			this->btnClear->Text = L"Clear";
+			this->btnClear->UseVisualStyleBackColor = true;
+			this->btnClear->Click += gcnew System::EventHandler(this, &MainForm::btnClear_Click);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->ClientSize = System::Drawing::Size(405, 470);
+			this->Controls->Add(this->tableLayoutPanel1);
+			this->Controls->Add(this->lbWelcome);
+			this->Controls->Add(this->tbLastName);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->tbFirstName);
+			this->Controls->Add(this->label1);
+			this->MinimumSize = System::Drawing::Size(423, 517);
+			this->Name = L"MainForm";
+			this->Text = L"Welcome";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void btnOK_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ firstName = this->tbFirstName->Text;
+		String^ lastName = this->tbLastName->Text;
+		this->lbWelcome->Text = "Hello " + firstName + " " + lastName;
+	}
+	private: System::Void btnClear_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->tbFirstName->Text = "";
+		this->tbLastName->Text = "";
+		this->lbWelcome->Text = "";
+	}
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
+		this->lbWelcome->Text = "";
+	}
+	};
+}
